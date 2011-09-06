635b9481badf89840c3ab1c996cff0e317bb0cdc
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-06 12:06:29 +0800

    Oops, fix a harmless typo in tests.

--- test/stylus_test.rb
+++ test/stylus_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../test_helper', __FILE__)
 
-class SqwishTest < UnitTest
+class StylusTest < UnitTest
   class App < Sinatra::Base
     set :root, File.expand_path('../app', __FILE__)
     register Sinatra::AssetPack
