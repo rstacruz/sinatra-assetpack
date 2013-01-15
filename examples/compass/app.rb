9590f96902c8f2be218a2855d3223ce380a89657
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-01-14 22:39:02 -0500

    Update compass example. Ref #23

--- examples/compass/app.rb
+++ examples/compass/app.rb
@@ -16,6 +16,15 @@ class App < Sinatra::Base
   # This is a convenient way of setting up Compass in a Sinatra
   # project without mucking around with load paths and such.
   register Sinatra::CompassSupport
+  set :sass, Compass.sass_engine_options
+  # Add optional load paths
+  set :sass, { :load_paths => [
+    "#{self.root}/app/css",
+    "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets",
+    "#{Gem.loaded_specs['compass-susy-plugin'].full_gem_path}/sass"
+  ]}
+  # Add scss support
+  # set :scss, sass
 
   # ### Compass sprite configuration
   # Skip this section if you don't need sprite images.
