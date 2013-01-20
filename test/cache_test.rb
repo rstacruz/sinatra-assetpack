9ae82da3775986bcbd19e17951519a4640e6a941
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-01-20 17:20:56 -0500

    Fix cache test

--- test/cache_test.rb
+++ test/cache_test.rb
@@ -1,8 +1,24 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class CacheTest < UnitTest
+  class App < UnitTest::App
+    register Sinatra::AssetPack
+
+    set :reload_templates, false
+    assets {
+      js :app, '/js/app.js', [
+        '/js/vendor/**/*.js',
+        '/js/assets/**/*.js',
+        '/js/hi.js',
+        '/js/hell*.js'
+      ]
+      js_compression :jsmin
+    }
+  end
+
+  def app() App; end
+
   test "Compressed js caching" do
-    app.set :reload_templates, false
     JSMin.expects(:minify).times(1).returns ""
     3.times { get '/js/app.js' }
   end
