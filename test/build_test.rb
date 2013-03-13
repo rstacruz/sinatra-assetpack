ffcb6d4511136cba884d23d9358c11a66645c9ab
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-03-13 17:54:00 -0400

    Add failing test for building cache issue. Ref #52

--- test/build_test.rb
+++ test/build_test.rb
@@ -1,13 +1,19 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class BuildTest < UnitTest
+  setup do
+    app.assets.css_compression = :simple
+    @source_path = File.join(app.root, 'app/js/hello.js')
+    @source_tmp = File.read(@source_path)
+  end
+
   teardown do
     FileUtils.rm_rf File.join(app.root, 'public')
+    File.open(@source_path, 'wb') { |f| f.write @source_tmp }
   end
 
   test "build" do
     Stylus.expects(:compile).returns("body{background:#f00;color:#00f;}")
-    app.assets.css_compression = :simple
     app.assets.build!
 
     assert File.file? File.join(app.root, 'public/js/app.js')
@@ -28,4 +34,18 @@ class BuildTest < UnitTest
       File.mtime(Dir["#{app.root}/public/images/background.*.jpg"].first).to_i ==
       File.mtime(Dir["#{app.root}/app/images/background.jpg"].first).to_i
   end
+
+  test "rebuilding" do
+    source_path = File.join(app.root, 'app/js/hello.js')
+    build_path = File.join(app.root, 'public/js/app.js')
+
+    app.assets.build!
+    assert File.read(build_path).include?('function(){alert("Hello");')
+
+    File.open(source_path, 'wb') { |f| f.write 'function(){alert("New");' }
+
+    app.assets.build!
+    assert File.read(build_path).include?('function(){alert("New");')
+  end
+
 end
